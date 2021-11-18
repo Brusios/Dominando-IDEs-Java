f7650d7c01c22446da704ee91e130f04096781a5
Brusios <brunogustavods@gmail.com>
2021-11-18 18:21:01 -0300

    First commit;

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import javax.swing.text.html.ListView;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*
+		 * int a = 2; int b = 3; System.out.println("Hello world! " + (a + b));
+		 */
+
+		Gato gato = new Gato(null, null, null);
+		Livro livro = new Livro();
+
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String npag;
+}
